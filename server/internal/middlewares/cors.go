36a7805c650a49b9c550d5e65f8cd68e1cfa1f93
manasa-manoj-nbr <manasamanojnbr1145@gmail.com>
2025-10-22 21:23:38 +0530

    fix(cors): updated cors allowed origin in be

--- server/internal/middlewares/cors.go
+++ server/internal/middlewares/cors.go
@@ -7,7 +7,7 @@ import (
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// CORS Headers
-		w.Header().Set("Access-Control-Allow-Origin", "*") // Configure for your frontend
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // Configure for your frontend
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true") // For cookies
